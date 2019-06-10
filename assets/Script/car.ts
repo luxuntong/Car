ebde1a34de230398a25a08730742ff040b1ac681
shanlihou <shanlihou@gmail.com>
2019-06-10 22:57:51 +0800

    up

--- assets/Script/car.ts
+++ assets/Script/car.ts
@@ -17,21 +17,44 @@ export class Car extends cc.Component {
         }
         // init logic
     }
+    onCollisionEnter (other, self) {
+        //console.log('crash:', other, self);
+    }
 
-    update (dt) {
+    update(dt) {
         this.move();
+    }
+    
+    onBeginContact(contact, selfCollider, otherCollider) {
+        console.log('on begin')
+    }
+
+    onPreSolve(contact, selfCollider, otherCollider) {
+        console.log('presolve:', contact, selfCollider, otherCollider)
     }
 
     public onJoyMove(vec: cc.Vec2, rate: number) {
-        this._moveDir = vec;
-        this._movespeed = rate;
+        if (rate)
+            this._moveDir = vec;
+            
+        let rigid = this.node.getComponent(cc.RigidBody)
+        //rigid.linearVelocity = vec.mul(rate * 100);
+        rigid.applyForceToCenter(vec.mul(rate * 100), true);
+        let box = this.node.getComponent(cc.BoxCollider);
+
+        let pos = this.node.convertToWorldSpace(this.node.position);
+        let collider = cc.director.getPhysicsManager().testPoint(this.node.position);
+        let p2 = this.node.position.add(vec.mul(1000));
+        var results = cc.director.getPhysicsManager().rayCast(this.node.position, p2, cc.RayCastType.Any);
+        console.log(results);
+
     }
 
     private move() {
         this.node.angle = 90 + cc.misc.radiansToDegrees(
             Math.atan2(this._moveDir.y, this._moveDir.x)
         );
-        let newPos = this.node.position.add(this._moveDir.mul(this._movespeed * 5));
-        this.node.setPosition(newPos);
+        //let newPos = this.node.position.add(this._moveDir.mul(this._movespeed * 5));
+        //this.node.setPosition(newPos);
     }
 }
--- assets/Script/world.ts
+++ assets/Script/world.ts
@@ -8,6 +8,12 @@ export default class World extends cc.Component {
 
     onLoad() {
         cc.director.getPhysicsManager().enabled = true;
+        cc.director.getCollisionManager().enabled = true;
+        /*
+        cc.director.getPhysicsManager().debugDrawFlags = cc.PhysicsManager.DrawBits.e_aabbBit |
+                                                        cc.PhysicsManager.DrawBits.e_jointBit |
+                                                        cc.PhysicsManager.DrawBits.e_shapeBit;
+                                                      */
         let first = cc.instantiate(this.carPrefab);
         let second = cc.instantiate(this.carPrefab);
         first.getComponent(Car).isplayer = true;
